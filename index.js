e915ca7289d9a1858aebce2e022cc4ee3c9aa501
Spencer Elliott <me@elliottsj.com>
2017-06-20 17:42:31 -0400

    Add ESLint and a test

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-'use strict'
+'use strict';
 
-var loaderUtils = require('loader-utils')
-var fm = require('front-matter')
+var loaderUtils = require('loader-utils');
+var fm = require('front-matter');
 
-module.exports = function (source) {
-	this.cacheable && this.cacheable()
-	var query = loaderUtils.parseQuery(this.query)
+module.exports = function(source) {
+  this.cacheable && this.cacheable();
+  var query = loaderUtils.parseQuery(this.query);
 
-	var frontmatter = fm(source)
-	if (query.onlyAttributes) {
-		return JSON.stringify(frontmatter.attributes)
-	} else if (query.onlyBody) {
-		return frontmatter.body
-	} else {
-		return JSON.stringify(frontmatter)
-	}
-}
+  var frontmatter = fm(source);
+  if (query.onlyAttributes) {
+    return JSON.stringify(frontmatter.attributes);
+  } else if (query.onlyBody) {
+    return frontmatter.body;
+  } else {
+    return JSON.stringify(frontmatter);
+  }
+};
