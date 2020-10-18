cf0c51634496a46f2f9ee8df4848c6f21f3fc91f
Spencer Elliott <me@elliottsj.com>
2020-10-18 16:21:29 -0400

    webpack v5 compatibility

--- example/example.test.js
+++ example/example.test.js
@@ -5,9 +5,12 @@ const config = require('./webpack.config');
 test('extracts frontmatter and body as an object', () =>
   new Promise((resolve) => {
     webpack(config, (error, stats) => {
+      if (error) {
+        throw error;
+      }
       const bundlePath = path.resolve(
         stats.compilation.compiler.outputPath,
-        stats.toJson().assetsByChunkName.main
+        stats.toJson().assetsByChunkName.main[0]
       );
       expect(require(bundlePath)).toMatchInlineSnapshot(`
         Object {
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
 
-var loaderUtils = require('loader-utils');
 var fm = require('front-matter');
 
 module.exports = function (source) {
   this.cacheable && this.cacheable();
-  var options = loaderUtils.getOptions(this) || {};
+  var options = this.getOptions() || {};
 
   var frontmatter = fm(source);
   if (options.onlyAttributes) {
